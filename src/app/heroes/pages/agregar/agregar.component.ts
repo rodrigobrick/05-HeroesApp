b65fa3bafc0097899fe3a786c34df415fa8c67fc
rodrigobrick <rodrigobrick25@gmail.com>
2022-02-23 12:05:24 -0300

    update

---
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [`
+    img{
+      width: 100%
+    }
+  `
+  ]
+})
+export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router,
+               private snackbar: MatSnackBar,
+               public dialog: MatDialog ) { }
+
+  ngOnInit(): void {
+
+    if(!this.router.url.includes('editar')){
+      return
+    }
+    
+    this.activatedRoute.params
+        .pipe( 
+          switchMap( ({id}) => this.heroesService.getHeroeId(id))
+        )
+        .subscribe( heroe => this.heroe = heroe)
+  }
+
+  guardar(){
+
+    if( this.heroe.superhero.trim().length === 0 ) {
+      return
+    }
+
+    if( this.heroe.id ){
+      this.heroesService.actualizarHeroe(this.heroe)
+          .subscribe( heroe => this.mostrarSnackBar('Registro actualizado'))
+    }
+    else {
+      this.heroesService.agregarHeroe(this.heroe)
+        .subscribe( heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id]);
+          this.mostrarSnackBar('Registro creado');
+        })
+    }
+  }
+
+  borrar(){
+
+    const dialog = this.dialog.open(ConfirmarComponent, {
+      width: '350px',
+      data: this.heroe
+    });
+
+    dialog.afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.heroesService.borrarHeroe( this.heroe.id!)
+              .subscribe( resp =>{
+          this.router.navigate(['/heroes'])
+        })
+        }
+      }
+    )
+  }
+
+  mostrarSnackBar( mensaje: string){
+
+    this.snackbar.open(mensaje, 'Cerrar', {
+      duration: 2000,
+    })
+  }
+  
+
+}
